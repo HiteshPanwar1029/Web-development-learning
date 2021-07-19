16ad7a9c220c2e375fac5695c2bc5f9fb88f1de9
Hitesh Panwar <67824569+HiteshPanwar1029@users.noreply.github.com>
2021-07-19 15:59:00 +0530

    Added Javascript functions

---
+++ Javascript functions.js
@@ -0,0 +1,12 @@
+console.log('functions');
+
+let name0 = "Hitesh"
+let name1 = "Prateek"
+let name2 = "Saransh"
+function greet(name){
+    console.log(name+" is using this site");
+}
+
+greet(name0);
+greet(name1);
+greet(name2);
