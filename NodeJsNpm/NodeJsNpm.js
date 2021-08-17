7d01559be438e9cce4428281629992f21a6d497b
Hitesh Panwar <67824569+HiteshPanwar1029@users.noreply.github.com>
2021-08-17 18:14:30 +0530

    Added NodeJsNpm

---
+++ NodeJsNpm/NodeJsNpm.js
@@ -0,0 +1,11 @@
+console.log("This is npm introduction");
+
+// To make package.json file
+// open terminal
+// type cd NodeJsNpm(path)
+// type npm init
+
+// --global last mai likhne se us package ko kahi bhi use kar sakte hai
+// npm install nodemon --global
+
+// https://www.youtube.com/watch?v=YBwWGvdUbQU&list=PLu0W_9lII9agiCUZYRsvtGTXdxkzPyItg&index=69&t=84s
